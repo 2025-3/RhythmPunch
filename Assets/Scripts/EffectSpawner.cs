8191ab457db71ae199e5bcd4c8f0dacce3e143e3
Kwon hyeonsoo <172839yes@naver.com>
2025-07-19 11:38:18 +0900

    eventsapw

---
+++ Assets/Scripts/EffectSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectSpawner : MonoBehaviour
+{
+    public static EffectSpawner Instance;
+
+    public GameObject missEffect;
+    public GameObject badEffect;
+    public GameObject goodEffect;
+    public GameObject perfectEffect;
+
+    public GameObject spawnPoint;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    
+    public void SpawnEffect(JudgementType judgementType)
+    {
+        GameObject v;
+
+        if (judgementType == JudgementType.Miss)
+            v = Instantiate(missEffect, spawnPoint.transform);
+        else if (judgementType == JudgementType.Bad)
+            v = Instantiate(badEffect, spawnPoint.transform);
+        else if (judgementType == JudgementType.Good)
+            v = Instantiate(goodEffect, spawnPoint.transform);
+        else 
+            v = Instantiate(perfectEffect, spawnPoint.transform);
+
+        Destroy(v, 2);
+    }
+}
