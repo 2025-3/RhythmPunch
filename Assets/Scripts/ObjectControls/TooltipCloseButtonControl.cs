0a65e522eceb86f4cd53512abcb288d65489b8c3
darkpppet <ruldaiz@naver.com>
2025-07-20 11:22:35 +0900

    .

--- Assets/Scripts/ObjectControls/TooltipCloseButtonControl.cs
+++ Assets/Scripts/ObjectControls/TooltipCloseButtonControl.cs
@@ -6,6 +6,22 @@ public class TooltipCloseButtonControl : MonoBehaviour
 {
     public GameObject tooltip;
 
+    public GameObject winPanel;
+    public GameObject losePanel;
+    
+    private void Awake()
+    {
+        GameManager.Instance.onWinGame.AddListener(() =>
+        {
+            winPanel.SetActive(true);
+        });
+        
+        GameManager.Instance.onEndGame.AddListener(() =>
+        {
+            losePanel.SetActive(true);
+        });
+    }
+    
     public void OnClick()
     {
         tooltip.SetActive(false);
